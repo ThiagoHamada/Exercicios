8b358a8e313065c74d9d4f0f006c43eafde0d88d
ThiagoHamada <thiago.lyu.hamada@gmail.com>
2023-05-22 19:01:10 -0300

    correcao

--- Reajuste.java
+++ Reajuste.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Reajuste {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite um valor");
