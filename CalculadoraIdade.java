113abe5fa147ce60c7a6c1972e6ae97bdd31e5c4
ThiagoHamada <thiago.lyu.hamada@gmail.com>
2023-05-29 11:18:10 -0300

    correcao

--- CalculadoraIdade.java
+++ CalculadoraIdade.java
@@ -7,7 +7,7 @@ public class CalculadoraIdade {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        try {
+       /* try {
             System.out.print("Digite a quantidade de anos: ");
             int anos = scanner.nextInt();
 
@@ -19,11 +19,22 @@ public class CalculadoraIdade {
 
             int totalDias = (anos * 365) + (meses * 30) + dias;
 
-            System.out.println("A idade em dias é: " + totalDias + " dias");
+            System.out.printf("A idade em dias é: %d dias", totalDias);
+        } catch (InputMismatchException e) {
+        System.out.println("Entrada inválida. Certifique-se de digitar apenas números inteiros."); */
+
+        try {
+            System.out.print("Digite sua Idade:");
+            int idade = scanner.nextInt();
+
+            int totalmes = idade * 12;
+            int totaldias = idade * 365;
+
+            System.out.printf("A idade em meses é %d e em dias é: %d dias.%n", totalmes, totaldias);
         } catch (InputMismatchException e) {
             System.out.println("Entrada inválida. Certifique-se de digitar apenas números inteiros.");
+        } finally {
+            scanner.close();
         }
-
-        scanner.close();
     }
 }
